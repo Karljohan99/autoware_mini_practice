cd87c3cdde56e1cb59f764a8a85962d7ee9ba724
Linux User <linuxuser@linux-d2006-11.domenis.local>
2024-02-13 15:09:06 +0200

    Added launch file and a new node

--- practice_1/nodes/publisher.py
+++ practice_1/nodes/publisher.py
@@ -4,9 +4,11 @@ import rospy
 from std_msgs.msg import String
 
 rospy.init_node('publisher')
-rate = rospy.Rate(2)
+rate_param = rospy.get_param('~rate', '2')
+rate = rospy.Rate(int(rate_param))
+message = rospy.get_param('~message', 'Hello World!')
 pub = rospy.Publisher('/message', String, queue_size=10)
 
 while not rospy.is_shutdown():
-    pub.publish("Hello world!")
+    pub.publish(message)
     rate.sleep()
